53a8db402fae40eda4cd25574e243bbfd049eceb
Rob Thomson <rob@marotori.com>
2025-10-13 12:50:53 +0100

    Update sound files / flight mode messaging

--- bin/sound-generator/generate-all.bat
+++ bin/sound-generator/generate-all.bat
@@ -4,7 +4,7 @@ setlocal
 REM Official voices can be found here:  https://github.com/FrSkyRC/ETHOS-Feedback-Community/blob/1.6/tools/audio_packs.json
 
 REM English - Default
-python generate-googleapi.py --only-missing --csv en.csv --voice en-US-Wavenet-F --base-dir en --variant default --engine google 
+python generate-googleapi.py --csv en.csv --voice en-US-Wavenet-F --base-dir en --variant default --engine google 
 
 
 REM copy just english to the release folder
